a9d3929f76e95167e70a98ff081b9d50ee60172e
Karl Holt <kholt19@student.aau.dk>
2020-02-19 19:46:02 +0100

    input fields now works, and change p

--- Brain.js
+++ Brain.js
@@ -1,14 +1,13 @@
 function simulator_start() {
-    let b = 0;
-    // alert("button was clicked");
-    let short_cut = document.getElementById("stats1").innerHTML;
-    console.log(short_cut);
-    
-    let thirst_start = document.getElementById("thirst").value;
-    let hunger_start = document.getElementById("hunger").value;
-    let speed_start = document.getElementById("speed").value;
-    let hp_start = document.getElementById("hp").value;
-    document.getElementById("stats1").innerHTML = thirst_start;
+    let thirst_start = Number(document.getElementById("thirst").value);
+    let hunger_start = Number(document.getElementById("hunger").value);
+    let speed_start = Number(document.getElementById("speed").value);
+    let hp_start = Number(document.getElementById("hp").value);
+
+    document.getElementById("stats1").innerHTML = "Maximum HP on rabbits: " + (hp_start > 0 ? hp_start : 10);
+    document.getElementById("stats2").innerHTML = "Maximum speed for rabbits: " + (speed_start > 0 ? speed_start : 10);
+    document.getElementById("stats3").innerHTML = "minimum hunger limit: " + (hunger_start > 0 ? hunger_start : 10);
+    document.getElementById("stats4").innerHTML = "minimum thirst limit: " + (thirst_start > 0 ? thirst_start : 10);
      
     /*
     if(short_cut == "her kan du analyser på hvad du har"){
